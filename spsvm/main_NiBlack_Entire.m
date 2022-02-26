a14bcb473fcab3493016cc588e0a688fac8b74d5
MIP2019 <93017628+MIP2019@users.noreply.github.com>
2022-02-26 21:21:56 +0800

    Add files via upload

---
+++ spsvm/main_NiBlack_Entire.m
@@ -0,0 +1,22 @@
+clear all
+close all
+%% https://blog.csdn.net/carson2005/article/details/14450561?_t=t
+
+path='C:\Mafei\Paper\1-SVM-Patch-LaTeX\1-Reply\Experiments\Original_Image\'
+dirFiles=dir(strcat(full(path),'*.tif'))
+saveDir=strcat('C:\Mafei\Paper\1-SVM-Patch-LaTeX\1-2-Reply\Q5_AdaptiveThreshold\NiBlack_5\')
+if ~exist(saveDir)
+    mkdir(saveDir)
+end
+windows=128
+for fi= 1:size(dirFiles,1)
+    
+    tf=imread(strcat(path,dirFiles(fi).name));    
+    imshow(tf)
+    tf=double(tf);
+    tf=medfilt2(tf,[15,15]);
+    gb2=segNiBlack(tf,windows,0.1);
+    gb2=medfilt2(gb2,[5,5]);
+%     figure();imshow(255-255*gb2,[]);
+     imwrite(255-255*gb2,strcat(saveDir,dirFiles(fi).name(1:end-4),'.bmp'))
+end
